987b453041f72f436a61699cd45b7cd8fc22b030
nmasahiro <masahironomura5325@gmail.com>
2018-10-17 17:36:18 +0900

    delete redundant import

--- src/main/scala/com/github/nmasahiro/kf/StateSpaceModel.scala
+++ src/main/scala/com/github/nmasahiro/kf/StateSpaceModel.scala
@@ -1,7 +1,5 @@
 package com.github.nmasahiro.kf
 
-import breeze.linalg.DenseMatrix
-
 case class StateSpaceModel(
   F: M, // linear factor for x in system model
   G: M, // linear factor for v in system model
